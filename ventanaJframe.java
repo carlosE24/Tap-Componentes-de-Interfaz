f2ffcbca3deca182cc4639f2dd3494a6d9cc92e6
Mannlex Murillo Macias <Mannlex21@MacBook-Pro-de-Mannlex.local>
2016-02-01 17:34:50 -0700

    Segunda version

--- ventanaJframe.java
+++ ventanaJframe.java
@@ -1,7 +1,12 @@
 import javax.swing.JFrame;
 public class ventanaJframe{
 public ventanaJframe (String titulo){
-
+JFrame ventana = new JFrame(titulo);
+ventana.setVisible(true);
+ventana.setSize(500,500);
+ventana.setLocationRelativeTo(null);
+ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ventana.setResizable(false);
 }
 public static void main (String []args){
 ventanaJframe v = new ventanaJframe("Interfaz");
