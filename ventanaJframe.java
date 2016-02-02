5d0ee3889b2dd0c26c8a7007d20a820a49ed6ea3
Mannlex Murillo Macias <Mannlex21@MacBook-Pro-de-Mannlex.local>
2016-02-01 17:15:12 -0700

    Primera version

---
+++ ventanaJframe.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class ventanaJframe{
+public ventanaJframe (String titulo){
+
+}
+public static void main (String []args){
+ventanaJframe v = new ventanaJframe("Interfaz");
+}
+}
